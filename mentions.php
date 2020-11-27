b6991eb4557b511c3927c120092e0914b0afaa94
Adrien Lapasset <aflapasset@gmail.com>
2020-11-27 17:23:51 +0100

    relative paths

--- mentions.php
+++ mentions.php
@@ -7,7 +7,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <title>CEKEDUBONHEUR</title>
   <meta name="description" content="">
-  <link rel="icon" type="image/png" href="/assets/imgs/favicon.png">
   <link rel="stylesheet" href="modules/bootstrap-4.5.3/dist/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
