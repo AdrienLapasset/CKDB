dd492f8dd0caf3cafe994d03b88ef76d653b5fe1
Adrien Lapasset <aflapasset@gmail.com>
2020-11-27 12:20:21 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+  <a href="">Modalités</a> -
+  <a href="">Mentions légales</a> -
+  <a href="">Contacts</a>
+</footer>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <title>Hasbro</title>
+  <meta name="description" content="">
+  <link rel="icon" type="image/png" href="/assets/imgs/favicon.png">
+  <link rel="stylesheet" href="modules/bootstrap-4.5.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+  <?php include 'header.php'; ?>
+  <main>
+    <img class="teasing" src="assets/imgs/teasing.png" alt="">
+  </main>
+  <?php include 'footer.php'; ?>
+</body>
+
+<!-- JS Imports -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="modules/bootstrap-4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</html>
