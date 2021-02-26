d0b9109268ba79e47af236e9ada0e98a62afcc7b
Adrien Lapasset <aflapasset@gmail.com>
2021-02-26 15:06:17 +0100

    update modalités

--- modalites.php
+++ modalites.php
@@ -18,42 +18,36 @@
     <div class="container">
       <article>
         <p class="text-center">
-          MODALITÉS DE L’OFFRE HASBRO ETRENNES EN ASSOCIATION AVEC CEKEDUBONHEUR<br>
+        MODALITÉS DE L’OFFRE HASBRO PAQUES EN ASSOCIATION AVEC CEKEDUBONHEUR<br>
           « JOUEZ C’EST QUE DU BONHEUR »
         </p>
-
         <h2>
           ARTICLE 1 – Société Organisatrice
         </h2>
-
         <p>
-          La société HASBRO FRANCE, S.A.S au capital de 1.700.000 Euros, immatriculée au Registre du Commerce et des Sociétés de Chambéry sous le n° 746 220 623 dont le siège social est situé à l’adresse suivante Savoie-Technolac, 73370 Le Bourget-du-Lac (ci-après désignée la "Société Organisatrice") propose une offre systématique avec obligation d’achat du 26/12 2020 au 31/01/2021 (ci-après désigné "L’Offre").
+        La société HASBRO FRANCE, S.A.S au capital de 1.700.000 Euros, immatriculée au Registre du Commerce et des Sociétés de Chambéry sous le n° 746 220 623 dont le siège social est situé à l’adresse suivante Savoie-Technolac, 73370 Le Bourget-du-Lac (ci-après désignée la "Société Organisatrice") propose une offre systématique avec obligation d’achat du 08/03 2021 au 18/04/2021 (ci-après désigné "L’Offre").
         </p>
-
         <h2>
           ARTICLE 2 – Conditions d’accès à l’Offre
         </h2>
-
         <p>
-          L’Offre est ouverte aux personnes physiques majeures, résidant en France Métropolitaine, Corse incluse, Andorre et Monaco et disposant d’un accès Internet et d’une adresse électronique valide. L’Offre est portée à la connaissance du public sur les supports suivants : PLV magasins (présentoirs, têtes de gondole, totems) et site internet de l’opération : www.hasbro-ckdb.fr (ci-après le "Site").
+        L’Offre est ouverte aux personnes physiques majeures, résidant en France Métropolitaine, Corse incluse, Andorre et Monaco et disposant d’un accès Internet et d’une adresse électronique valide. L’Offre est portée à la connaissance du public sur les supports suivants : PLV magasins (présentoirs, têtes de gondole, totems) et site internet de l’opération : www.hasbro-ckdb.fr (ci-après le "Site"). 
         </p>
-
         <h2>
           ARTICLE 3 – Modalités de participation
         </h2>
-
         <p>
-          L’Offre se présente de la manière suivante :
-          1 jouet Hasbro éligible acheté = 1 jouet offert à un enfant hospitalisé via l’association Cekedubonheur.
-          Le jouet sera choisi par l’association en fonction des besoins des enfants hospitalisés.
-          Offre valable dans la limite de 10 000 dons.
+          L’Offre se présente de la manière suivante :<br>
+          2 jouets Hasbro éligibles achetés = 1 activité offerte à un enfant hospitalisé via l’association Cékedubonheur.
         </p>
-
+        <p>
+          Il a été défini avec l’association Cékedubonheur que les activités seraient des places de cinéma que les psychologues des 104 hôpitaux partenaires redistribueraient aux familles des enfants hospitalisés. L’offre est valable dans la limite de 10 000 dons ; soit 10 000 places ciné-chèques offertes. 
+        </p>
         <p>
           Pour accéder à l’Offre, les participants devront :
           <ul>
             <li>
-              1- Acheter entre le 26/12/2020 et le 31/01/2021 (ticket de caisse faisant foi, il pourra être demandé en cas de doute) un produit Hasbro éligible parmi les gammes de produits Hasbro suivantes :
+              1- Acheter entre le 08/03 2021 au 18/04/2021 (ticket de caisse faisant foi, il pourra être demandé en cas de doute) deux produits Hasbro éligibles parmi les gammes de produits Hasbro suivantes : 
               <ul>
                 <li>
                   • Nerf
@@ -70,57 +64,42 @@
               </ul>
             </li>
             <li>
-
-              2- Se connecter sur le site www.hasbro-ckdb.fr entre le 26/12/2020 et le 31/01/2021 inclus (date et heures en France Métropolitaine) muni du code-barres du produit acheté.
+              2- Se connecter sur le site www.hasbro-ckdb.fr entre le 08/03 2021 au 30/04/2021 inclus (date et heures en France Métropolitaine) muni du code-barres des produits achetés.
             </li>
-
             <li>
-
-              3- S'inscrire sur le Site en remplissant les champs obligatoires du formulaire de souscription à l’Offre à savoir l’adresse email* et le code barre du produit*
+              3- S'inscrire sur le Site en remplissant les champs obligatoires du formulaire de souscription à l’Offre à savoir l’adresse email* et le code-barres des produits achetés*
             </li>
-
             <li>
-
               4- Lire et accepter les présentes modalités et conditions de l’Offre en cochant la case prévue à cet effet.
             </li>
-
             <li>
-
               5- Accepter que les données personnelles soient utilisées dans le cadre de l’Offre.
             </li>
 
             <li>
 
               6- Valider le formulaire.
-              Le participant recevra un email de confirmation pour le remercier et l’avertir que son produit a bien été offert à l’association Cekedubonheur.
             </li>
-
+           <p>Le participant recevra un email de confirmation pour le remercier et l’avertir que son produit a bien été offert à l’association Cekedubonheur.</p> 
           </ul>
-
         </p>
-
         <h2>
           ARTICLE 4 – Propriété intellectuelle
         </h2>
-
         <p>
-          Toutes les marques, logos et autres signes distinctifs composant l’Offre sont la propriété exclusive de leurs titulaires et sont protégés à ce titre par les dispositions du Code de la propriété intellectuelle. La reproduction et la représentation de tout ou partie de ces éléments sont strictement interdites et constituent une contrefaçon passible de sanctions pénales.
+        Toutes les marques, logos et autres signes distinctifs composant l’Offre sont la propriété exclusive de leurs titulaires et sont protégés à ce titre par les dispositions du Code de la propriété intellectuelle. La reproduction et la représentation de tout ou partie de ces éléments sont strictement interdites et constituent une contrefaçon passible de sanctions pénales. 
         </p>
-
         <h2>
           ARTICLE 5 – Protection des données à caractère personnel
         </h2>
-
         <p>
-          Les coordonnées des participants seront traitées conformément à la loi Informatique et Libertés du 6 janvier 1978 et au RGPD. Chaque participant dispose d'un droit d'accès, de rectification, d’opposition ou de suppression des informations le concernant qui pourra être exercé auprès de la Société Organisatrice à l’adresse email dataprivacy@hasbro.com.
-          Les informations qui sont collectées dans le cadre de la présente Offre par la Société Organisatrice et son prestataire de services auront pour seule finalité la gestion de l’Offre et aucunement des fins commerciales ou autres. Elles ne seront pas conservées par la société organisatrice au-delà du délai nécessaire au traitement de l’Offre.
-          En cas de problème lié à l’utilisation de leurs données, les participants peuvent introduire une réclamation auprès de la CNIL.
-        </p>
-
+        Les coordonnées des participants seront traitées conformément à la loi Informatique et Libertés du 6 janvier 1978 et au RGPD. Chaque participant dispose d'un droit d'accès, de rectification, d’opposition ou de suppression des informations le concernant qui pourra être exercé auprès de la Société Organisatrice à l’adresse email dpo@take-off.fr et à l’adresse postale :        </p>
+        <p class="text-center">TAKE OFF – SERVICE DPO<br>CS 50454<br>13096 Aix en Provence CEDEX 2</p>
+        <p>Les informations qui sont collectées dans le cadre de la présente Offre par la Société Organisatrice et son prestataire de services auront pour seule finalité la gestion de l’Offre et aucunement des fins commerciales ou autres. Les données sont conservées pour une durée d’un an à compter de la clôture de cette opération. </p>
+        <p>En cas de problème lié à l’utilisation de leurs données, les participants peuvent introduire une réclamation auprès de la CNIL.</p>
         <h2>
           ARTICLE 6 – Dépôt et acceptation des modalités
         </h2>
-
         <p>
           Les présentes modalités sont déposées auprès de Maître GONIN, huissier de justice à Lyon, résidant au 139 rue Vendôme 69006 LYON.
         </p>
